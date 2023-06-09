5c06f33f0f195e7e1688cba414d963ecfff49652
YuichiroMuta <yumupeace86@g.ecc.u-tokyo.ac.jp>
2023-06-10 02:33:48 +0900

    change files

--- my-app-ts/src/Mypage.tsx
+++ my-app-ts/src/Mypage.tsx
@@ -43,7 +43,7 @@ const Mypage: React.FC = () => {
                   チャンネル選択は<Link className="slack-mypage__link" to={`/getUserChannels/`}>こちら</Link>
                 </p>
                 <p>
-                  新しくチャンネルに入りたい・作りたい方は<Link className="slack-mypage__link" to={`https://forms.gle/nCVQLxewJPEHywD48`}>こちら</Link>
+                  新しくチャンネルの作成・参加は<Link className="slack-mypage__link" to={`https://forms.gle/nCVQLxewJPEHywD48`}>こちら</Link>
                 </p>
               </div>
             </>
@@ -55,55 +55,3 @@ const Mypage: React.FC = () => {
 
 export default Mypage;
 
-/*
-import React, { useState, useEffect } from "react";
-import { onAuthStateChanged, signOut } from "firebase/auth";
-import { auth } from "./firebase";
-import { useNavigate, Navigate, Link } from "react-router-dom";
-
-
-const Mypage: React.FC = () => {
-  const [user, setUser] = useState<any>("");
-  const [loading, setLoading] = useState(true);
-  
-  useEffect(() => {
-    onAuthStateChanged(auth, (currentUser) => {
-      setUser(currentUser);
-      setLoading(false);
-    });
-  }, []);
-
-  const navigate = useNavigate();
-
-  const logout = async () => {
-    await signOut(auth);
-    navigate("/login/");
-  };
-
-
-  //ボタン(「チャンネルを選択」)を押すとemail情報をサーバに送り、getuserchannelコンポーネントを処理するようにしたい
-
-  return (
-    <>
-      {!loading && (
-        <>
-          {!user ? (
-            <Navigate to={`/login/`} />
-          ) : (
-            <>
-              <h1>マイページ</h1>
-              <p>{user && user.email}</p>
-              <button onClick={logout}>ログアウト</button>
-              <p>
-              チャンネル選択は<Link to={`/getUserChannels/`}>こちら</Link>
-            </p>
-            </>
-          )}
-        </>
-      )}
-    </>
-  );
-};
-
-export default Mypage;
-*/
